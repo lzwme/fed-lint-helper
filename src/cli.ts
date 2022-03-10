cf3d5914d7a74e9f7f5188f16db9113535aff3d3
任侠 <lzwy0820@qq.com>
2022-03-10 17:18:13 +0800

    fix: 修正更新 commander 后 cli 参数默认值错误

--- src/cli.ts
+++ src/cli.ts
@@ -2,7 +2,7 @@
  * @Author: lzw
  * @Date: 2021-09-25 15:45:24
  * @LastEditors: lzw
- * @LastEditTime: 2022-01-11 10:30:54
+ * @LastEditTime: 2022-03-10 17:16:29
  * @Description: cli 工具
  */
 import { Option, program } from 'commander';
@@ -45,12 +45,12 @@ program
   .option('--silent', `开启静默模式。`, false)
   .option('--debug', `开启调试模式。`, false)
   .addOption(new Option('--mode <mode>', `执行模式。`).choices(['current', 'proc', 'thread']))
-  .option('--no-print-detail', `不打印异常详情。`, false)
+  .option('--no-print-detail', `不打印异常详情。`)
   .option('--src <src...>', `指定要检测的源码目录。默认为 src`)
   .option('--only-changes', `只检测 git 仓库变更的文件`, false)
   .option('--cache', `开启缓存模式。`, false)
   .option('--remove-cache', `移除已存在的缓存。`)
-  .option('--no-exit-on-error', `检测到异常时，不以非 0 值立即退出。`, false)
+  .option('--no-exit-on-error', `检测到异常时，不以非 0 值立即退出。`)
   .option('--toWhiteList', `是否将检测到异常的文件输出到白名单文件列表中。`, false)
   .option('--tscheck', `执行 TypeScript Diagnostics check`)
   .option('--eslint', `执行 eslint 检查`)
@@ -63,13 +63,13 @@ program
   .option('--jest', `执行 jest 单元测试`)
   .action((opts: POptions) => {
     const options: FlhConfig = {
-      exitOnError: opts.exitOnError,
+      exitOnError: opts.exitOnError !== false,
       cache: !!opts.cache,
       removeCache: opts.removeCache,
       checkOnInit: false,
       silent: !opts.debug && opts.silent,
       debug: opts.debug,
-      printDetail: opts.printDetail,
+      printDetail: opts.printDetail !== false,
       mode: opts.mode || 'proc',
       tscheck: {
         toWhiteList: opts.toWhiteList,
