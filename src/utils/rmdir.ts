2fa5f13cb2158fe5ae31898bdc2eb67fe2c16e42
renxia <lzwy0820@qq.com>
2022-05-21 11:43:57 +0800

    perf: rm 删除文件夹命令支持 glob 匹配

--- src/utils/rmdir.ts
+++ src/utils/rmdir.ts
@@ -2,8 +2,10 @@ import fs from 'fs';
 import path from 'path';
 import { color } from 'console-log-colors';
 import { readSyncByRl } from './common';
+import { glob } from 'glob';
+import { getLogger } from './get-logger';
 
-function rmdirCustom(dirname: string) {
+function rmRecursive(dirname: string) {
   if (!fs.existsSync(dirname) || !fs.statSync(dirname).isDirectory()) return;
 
   const fileList = fs.readdirSync(dirname);
@@ -15,7 +17,7 @@ function rmdirCustom(dirname: string) {
     const fileStat = fs.statSync(filePath);
 
     if (fileStat.isDirectory()) {
-      rmdirCustom(filePath);
+      rmRecursive(filePath);
       continue;
     }
 
@@ -25,35 +27,55 @@ function rmdirCustom(dirname: string) {
 }
 
 async function doRmdir(source: string, slient = false, force = false) {
-  // console.log(option);
-  if (!source) return console.log('请指定要删除的文件或目录路径');
-  source = path.resolve(source);
-  if (!fs.existsSync(source)) return console.log('要删除的文件或目录路径不存在！', color.red(source));
+  const logger = getLogger();
+
+  // if (!source) return logger.warn('请指定要删除的文件或目录路径');
+  // source = path.resolve(source);
+  // if (!fs.existsSync(source)) return logger.warn('要删除的文件或目录路径不存在！', color.red(source));
+
   const sourceTip = fs.statSync(source).isFile() ? '文件' : '目录';
 
   if (!force) {
     const force = await readSyncByRl(`是否删除该${sourceTip}(y/)？[${color.red(source)}] `);
-    if ('y' !== String(force).trim().toLowerCase()) return;
+    if ('y' !== String(force).trim().toLowerCase()) return false;
   }
-  try {
-    fs.rmSync(source, { recursive: true });
-  } catch (error) {
-    console.log('error:', error.message || error);
-    rmdirCustom(source);
-  }
-  if (!slient) console.log(`${sourceTip}已删除：`, color.green(source));
+
+  await new Promise(resolve => {
+    fs.rm(source, { recursive: true, maxRetries: 3 }, error => {
+      if (error) {
+        console.log('error:', error.message || error);
+        rmRecursive(source);
+      }
+      resolve(error?.errno);
+    });
+  });
+
+  if (!slient) logger.info(`${sourceTip}已删除：`, color.green(source));
   return true;
 }
 
 export async function rmdir(srcs: string[], slient = false, force = false) {
-  if (!Array.isArray(srcs) || srcs.length === 0) return console.log('请指定要删除的文件或目录路径');
-  if (srcs.length === 1) return doRmdir(srcs[0], slient, force);
+  const logger = getLogger();
 
-  for (const source of srcs) {
-    await doRmdir(source, slient, force);
+  if (!Array.isArray(srcs) || srcs.length === 0) {
+    logger.warn('请指定要删除的文件或目录路径');
+    return 0;
   }
 
-  return true;
+  const list: Promise<boolean>[] = [];
+  for (const source of srcs) {
+    const files = glob.sync(source, { cwd: process.cwd() });
+    for (const filepath of files) {
+      list.push(doRmdir(filepath, slient, force));
+    }
+  }
+
+  const result = await Promise.all(list);
+  const total = result.filter(Boolean).length;
+
+  logger.debug(`执行完成！共删除了 ${total} 个文件或目录`);
+
+  return total;
 }
 
 if (module === require.main) rmdir(process.argv.slice(2), false, true);
