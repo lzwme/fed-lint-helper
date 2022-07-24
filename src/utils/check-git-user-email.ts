802f32d8d02800c99975ca69ccb10c309be1ccda
renxia <lzwy0820@qq.com>
2022-07-24 10:53:03 +0800

    wip: 更正提交者邮箱检测处理逻辑

--- src/utils/check-git-user-email.ts
+++ src/utils/check-git-user-email.ts
@@ -1,3 +1,4 @@
+import { color } from 'console-log-colors';
 import { existsSync, writeFileSync } from 'fs';
 import { resolve } from 'path';
 import { execSync } from './exec';
@@ -12,8 +13,8 @@ export const checkUserEmial = (regRule: string | RegExp, exitOnError = true, roo
   const email = execSync('git config --get user.email').stdout;
 
   if (typeof regRule === 'string') regRule = new RegExp(regRule);
-  if (regRule.test(email)) {
-    errmsg = `不正确的提交邮箱配置，正确的规则为：${regRule.toString()}。当前为：${email}`;
+  if (!regRule.test(email)) {
+    errmsg = `不正确的邮箱配置，正确的规则为：${color.green(regRule.toString())}。当前为：${color.red(email)}`;
     if (exitOnError) {
       console.error(errmsg);
       process.exit(-1);
